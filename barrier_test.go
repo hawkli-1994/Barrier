1961ff3ee551e242ad024c905cdf2542123a38d8
likeran <windflyme5@gmail.com>
2021-01-20 00:04:12 +0800

    test

--- barrier_test.go
+++ barrier_test.go
@@ -23,3 +23,39 @@ func TestBarrier_Wait(t *testing.T) {
 	}
 	wg.Wait()
 }
+
+func TestBarrier_Zero(t *testing.T) {
+	num := 0
+	barrier := New(num)
+	wg := sync.WaitGroup{}
+	for i := 0; i < num; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("A")
+			barrier.Wait()			//barrier.Init()
+			fmt.Println("B")
+			barrier.Wait()
+			fmt.Println("C")
+		}()
+	}
+	wg.Wait()
+}
+
+func TestBarrier_One(t *testing.T) {
+	num := 1
+	barrier := New(num)
+	wg := sync.WaitGroup{}
+	for i := 0; i < num; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("A")
+			barrier.Wait()			//barrier.Init()
+			fmt.Println("B")
+			barrier.Wait()
+			fmt.Println("C")
+		}()
+	}
+	wg.Wait()
+}
