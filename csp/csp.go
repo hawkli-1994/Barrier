9e42a11073e531ded68905346a1565b6e82cfd00
likeran <windflyme5@gmail.com>
2021-01-20 01:13:50 +0800

    csp风格的写法

---
+++ csp/csp.go
@@ -0,0 +1,47 @@
+package csp
+
+import (
+	"time"
+)
+
+type Barrier struct {
+	n        int
+	channels []chan int
+}
+
+func NewBarrier(n int) *Barrier {
+	channels := make([]chan int, n)
+	for i := 0; i < n; i++ {
+		channels[i] = make(chan int)
+	}
+	barrier := Barrier{
+		n:        n,
+		channels: channels,
+	}
+	go func() {
+		barrier.channels[0] <- 1
+	}()
+	return &barrier
+}
+
+func (barrier *Barrier) Wait() {
+	c := barrier.channels[0]
+	n := <-c
+	if n != barrier.n {
+		c <- n + 1
+		<-barrier.channels[n]
+	}
+	close(barrier.channels[n-1])
+}
+
+func main() {
+	b := NewBarrier(3)
+	for i := 1; i < 4; i++ {
+		go func(s int) {
+			time.Sleep(time.Duration(s) * time.Second)
+			b.Wait()
+			println(time.Now().Second())
+		}(i)
+	}
+	time.Sleep(time.Duration(5) * time.Second)
+}
